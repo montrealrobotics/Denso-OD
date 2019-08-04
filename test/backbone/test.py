262ccdccca739c896862ec95c02c0d44b92e16bd
dhaivat666 <dhaivat1994@gmail.com>
2019-08-04 06:25:17 -0400

    Script to test functionalities.

---
+++ test/backbone/test.py
@@ -0,0 +1,22 @@
+'''
+Program to test backbone network.
+'''
+
+import torch
+import sys
+
+## Inserting path of src directory
+sys.path.insert(1, '../..')
+
+from src.backbone import Backbone
+
+## Generate random input
+## TODO: replace with actual image later,with vision tranforms(normalization)
+input_image = torch.randn(1,3,600,600)
+
+backbone_obj = Backbone()
+out = Backbone(input_image)
+
+## Test resnet-101
+out = backbone_obj.resnet101_backbone(input_image)
+print(out.shape) 
