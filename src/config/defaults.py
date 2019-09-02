471c09216a95ecae722e399bd70519831e6e2e41
dhaivat666 <dhaivat1994@gmail.com>
2019-09-02 16:25:28 -0400

    Add input preprocessing params

--- src/config/defaults.py
+++ src/config/defaults.py
@@ -24,6 +24,31 @@ conf_params.BACKBONE.MODEL_NAME = 'resnet101'
 conf_params.BACKBONE.RESNET_STOP_LAYER = 4 
 
 
+##### Input config #####
+"""
+Params to define input image transformation.
+The input to backbone network has to be RGB 
+image with intensity scaled between 0 to 1.
+The following normalization is applied on the 
+top of that image. 
+
+The mean/std are standard used for training the 
+backbone networks. 
+
+Find more details here.
+https://github.com/pytorch/examples/blob/97304e232807082c2e7b54c597615dc0ad8f6173/imagenet/main.py#L197-L198
+https://discuss.pytorch.org/t/how-to-preprocess-input-for-pre-trained-networks/683
+https://pytorch.org/docs/stable/torchvision/models.html
+
+The mean and std are of ImageNet dataset, as the models are trained on that. 
+We will stick to same to get better output.
+"""
+
+conf_params.INPUT = CN()
+conf_params.INPUT.MEAN = 0.485, 0.456, 0.406
+conf_params.INPUT.STD = 0.229, 0.224, 0.225
+
+
 ##### ANCHOR CONFIG #####
 """
 Necessary params to define anchors
