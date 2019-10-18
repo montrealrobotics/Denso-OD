3c9596281cfcf0ba35145d774cd3a8ae5735d844
Dishank Bansal <dishankbansal09@gmail.com>
2019-10-17 20:15:05 -0400

    rpn changes

--- src/RPN/rpn.py
+++ src/RPN/rpn.py
@@ -23,68 +23,69 @@ class RPN(nn.Module):
 		self.conv1 = nn.Conv2d(self.in_channels, self.rpn_channels[0], 3, 1, 1)
 		self.conv1.weight.data.normal_(cfg.RPN.CONV_MEAN, cfg.RPN.CONV_VAR)
 		self.conv1.bias.data.fill_(cfg.RPN.BIAS)
-		
-		## Layer 2
-		self.conv2 = nn.Conv2d(self.rpn_channels[0], self.rpn_channels[1], 3, 1, 1)
-		self.conv2.weight.data.normal_(cfg.RPN.CONV_MEAN, cfg.RPN.CONV_VAR)
-		self.conv2.bias.data.fill_(cfg.RPN.BIAS)
-
-		## Layer 3
-		self.conv3 = nn.Conv2d(self.rpn_channels[1], self.rpn_channels[2], 3, 1, 1)
-		self.conv3.weight.data.normal_(cfg.RPN.CONV_MEAN, cfg.RPN.CONV_VAR)
-		self.conv3.bias.data.fill_(cfg.RPN.BIAS)
+		# nn.init.xavier_uniform(self.conv1.weight)
+		self.eLU1 = nn.ELU(alpha = cfg.RPN.ACTIVATION_ALPHA)
 
 		## Regression layer
-		self.reg_layer = nn.Conv2d(self.rpn_channels[2], self.n_anchors*4, 1, 1, 0)
+		self.reg_layer = nn.Conv2d(self.rpn_channels[0], self.n_anchors*4, 1, 1, 0)
 		self.reg_layer.weight.data.normal_(cfg.RPN.CONV_MEAN, cfg.RPN.CONV_VAR)
 		self.reg_layer.bias.data.fill_(cfg.RPN.BIAS)
+		# nn.init.xavier_uniform(self.reg_layer.weight)
+		self.eLU_reg = nn.ELU(alpha = cfg.RPN.ACTIVATION_ALPHA)
 
 		## classification layer
-		self.classification_layer = nn.Conv2d(self.rpn_channels[2], self.n_anchors*2, 1, 1, 0)
+		self.classification_layer = nn.Conv2d(self.rpn_channels[0], self.n_anchors*2, 1, 1, 0)
 		# self.softmax_classification = nn.Softmax(dim=2)
 		self.classification_layer.weight.data.normal_(cfg.RPN.CONV_MEAN, cfg.RPN.CONV_VAR)
 		self.classification_layer.bias.data.fill_(cfg.RPN.BIAS)
+		# nn.init.xavier_uniform(self.classification_layer.weight)
 
 		## Uncertainty layer
-		self.uncertain_layer = nn.Conv2d(self.rpn_channels[2], self.n_anchors*4, 1, 1, 0, bias=False)
+		self.uncertain_layer = nn.Conv2d(self.rpn_channels[0], self.n_anchors*4, 1, 1, 0)
 		self.uncertain_layer.weight.data.normal_(cfg.RPN.UNCERTAIN_MEAN, cfg.RPN.UNCERTAIN_VAR)
-		# self.uncertain_layer.bias.data.fill_(cfg.RPN.UNCERTAIN_BIAS)	## Initialize with high values to avoid NaNs
+		self.uncertain_layer.bias.data.fill_(cfg.RPN.UNCERTAIN_BIAS)	## Initialize with high values to avoid NaNs
+		# nn.init.xavier_uniform(self.uncertain_layer.weight)
+		self.softplus =  nn.Softplus(beta = cfg.RPN.SOFTPLUS_BETA, threshold = cfg.RPN.SOFTPLUS_THRESH)
+		# self.eLU_sigma = nn.ELU(alpha = cfg.RPN.ACTIVATION_ALPHA)
 
 
 		## Softplus for uncertainty
-		self.softplus =  nn.Softplus(beta = cfg.RPN.SOFTPLUS_BETA, threshold = cfg.RPN.SOFTPLUS_THRESH)
-		self.eLU = nn.ELU(alpha = cfg.RPN.ACTIVATION_ALPHA)
 
 
 	def forward(self, feature_map):
 
 		## forward pass
-		result = {}
+		# result = {}
 
 		## Layer 1 forward pass
-		x = self.eLU(self.conv1(feature_map))
+		x = self.eLU1(self.conv1(feature_map))
 
 		## Layer 2 forward pass
-		x = self.eLU(self.conv2(x))
+		# x = self.eLU(self.conv2(x))
 
-		## Layer 3 forward pass
-		x = self.eLU(self.conv3(x))
+		# ## Layer 3 forward pass
+		# x = self.eLU(self.conv3(x))
 
 		## Output of regression layer 
-		result['regression'] = self.eLU(self.reg_layer(x))
-
+		regress_out = self.eLU_reg(self.reg_layer(x))
+		regress_out = regress_out.view((regress_out.shape[0], 4, -1))
+		regress_out = regress_out.permite(0, 2, 1)
 		## Output of classification layer
 		# result['classification'] = self.softmax_classification(self.classification_layer(x))
-		result['classification'] = self.classification_layer(x)
+		class_out = self.classification_layer(x)
+		class_out = class_out.view((class_out.shape[0], 2, -1))
+		class_out = class_out.permute(0, 2, 1)
 
 		## Output of uncertainty layer
-		result['uncertainty'] = self.RichardCurve(self.uncertain_layer(x), low=0, high=10)
+		sigma_out = self.softplus(self.uncertain_layer(x))
+		sigma_out = sigma_out.view((sigma_out.shape[0], 4, -1))
+		sigma_out = sigma_out.permute(0,2,1)
 		
-		return self.reshape_output(result)
+		return [regress_out, class_out, sigma_out]
 
 
 	def RichardCurve(self, x, low=0, high=1, sharp=0.5):
-		r"""Applies the generalized logistic function (aka Richard's curve)
+		"""Applies the generalized logistic function (aka Richard's curve)
 		to the input tensor x.
 
 		Args:
@@ -107,7 +108,7 @@ class RPN(nn.Module):
 		If output of regression layer is 8x36x18x18, the final 
 		output will be of size 8x2916x4
 
-		If output of regression layer is 8x18x18x18, the final 
+		If output of classification layer is 8x18x18x18, the final 
 		output will be of size 8x2916x2(2916 = total number of anchors per image, 8 = batchsize)
 
 		'''
